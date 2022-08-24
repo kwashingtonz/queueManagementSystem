cf64ec580577e214b1c3d3479737acdea09749bf
kwashingtonz <kalinduwashington@gmail.com>
2022-08-24 09:54:23 +0530

    modified

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const http_1 = __importDefault(require("http"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     const message = "Hello World";
     res.json({ message: message });
 });
-app.listen("3001", () => {
-    console.log("Server Running...");
+server.listen(8000, () => {
+    console.log('app runing on server 8000');
 });
 //# sourceMappingURL=index.js.map
