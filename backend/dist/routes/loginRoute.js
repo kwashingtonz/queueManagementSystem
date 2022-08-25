14f19c1959d6eb818b391032fc6a7ca2ad455fe3
kwashingtonz <kalinduwashington@gmail.com>
2022-08-25 10:18:46 +0530

    modified

--- backend/dist/routes/loginRoute.js
+++ backend/dist/routes/loginRoute.js
@@ -3,9 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const loginController_1 = require("../controllers/loginController");
 const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.status(200).json({ success: "Login Page loaded" });
-});
 router.post('/', loginController_1.loginUser);
 exports.default = router;
 //# sourceMappingURL=loginRoute.js.map
--- backend/src/routes/loginRoute.ts
+++ backend/src/routes/loginRoute.ts
@@ -4,10 +4,6 @@ import { loginUser } from "../controllers/loginController"
 
 const router = Router();
 
-
-router.get('/',(req,res) =>{
-        res.status(200).json({success: "Login Page loaded"})
-    })
  
 router.post('/',loginUser)
 
